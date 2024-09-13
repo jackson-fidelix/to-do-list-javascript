53e33e5790be9cc322ef479daa038e99e1c5fd0c
Jackson Fidelix <jcksfidelix@gmail.com>
2024-09-13 14:52:11 -0300

    Update add Item in List

--- script.js
+++ script.js
@@ -11,5 +11,21 @@ function newTask() {
     else {
         // increment to localStorage
         let values = JSON.parse(localStorage.getItem('localStorageKey') || "[]")
+        values.push({
+            name: input.value
+        })
+         localStorage.setItem('localStorageKey',JSON.stringify(values))
+         showValues()
     }
 }
+
+function showValues(){
+    let values = JSON.parse(localStorage.getItem('localStorageKey') || "[]")
+    let list = document.getElementById('to-do-list')
+    list.innerHTML = ''
+    for(let i = 0; i < values.length; i++){
+        list.innerHTML += `<li>${values[i]['name']}</li>`
+    }
+}
+
+showValues()
