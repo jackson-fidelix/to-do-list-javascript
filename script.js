76fc993d6d37c81155c4539596f6bd60c58f4fd4
Jackson Fidelix <jcksfidelix@gmail.com>
2024-10-05 20:40:36 -0300

    Importants updates

--- script.js
+++ script.js
@@ -8,25 +8,40 @@ function validateNewTask(day) {
 }
 
 function newTask(day) {
-    // Captura o input de acordo com o dia da semana
     const taskInput = document.getElementById(`input-new-task-${day}`);
     const taskList = document.getElementById(`to-do-list-${day}`);
     
-    // Verifica se o input não está vazio
     if (taskInput.value.trim() !== "") {
-        // Cria um novo item de lista (li)
-        const newTaskItem = document.createElement("li");
-        newTaskItem.textContent = taskInput.value;
+        // Recupera a lista de tarefas do localStorage
+        let tasks = JSON.parse(localStorage.getItem(`to-do-list-${day}`) || "[]");
 
-        // Adiciona o novo item à lista de tarefas do dia
-        taskList.appendChild(newTaskItem);
+        //vamos verificar se existe a tarefa para aquele dia
+        let taskExists = tasks.find(task => task.name.toLowerCase() === taskInput.value.toLowerCase());
 
-        // Limpa o campo de input após adicionar
-        taskInput.value = "";
+        if (!taskExists){
+            // Adiciona a nova tarefa ao array
+            let newTask = { name: taskInput.value };
+            tasks.push(newTask);
+
+            // Atualiza o localStorage com a nova tarefa
+            localStorage.setItem(`to-do-list-${day}`, JSON.stringify(tasks));
+
+            // Exibe a nova tarefa na lista visual
+            const newTaskItem = document.createElement("li");
+            newTaskItem.textContent = taskInput.value;
+            taskList.appendChild(newTaskItem);
+
+            // Limpa o campo de input
+            taskInput.value = "";
+        }else {
+            alert('Essa tarefa já existe!')
+        }
     } else {
         alert("Por favor, insira uma tarefa.");
+        
     }
 }
+
 
 function showValues(day) {
     let values = JSON.parse(localStorage.getItem(`to-do-list-${day}`) || "[]");
@@ -38,7 +53,7 @@ function showValues(day) {
             <li>
                 ${values[i]['name']}
                 <button id="btn-ok" onclick="removeTask(${i}, '${day}')">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check-all" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-check-all" viewBox="0 0 16 16">
                         <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z"/>
                     </svg>
                 </button>
@@ -60,3 +75,5 @@ const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
 daysOfWeek.forEach(day => {
     showValues(day)
 });
+
+showValues(day);
