598bb938b806f2e5a8036e4ff0fa9bdd499cc61d
Jackson Fidelix <jcksfidelix@gmail.com>
2024-10-14 14:41:18 -0300

    Finished!!! Lasy Update

--- script.js
+++ script.js
@@ -70,20 +70,27 @@ function moveTask(index, day) {
     localStorage.setItem(`to-do-list-${day}`, JSON.stringify(tasks));
     localStorage.setItem('completed-tasks', JSON.stringify(completedTasks));
 
+    console.log(localStorage.getItem('completed-tasks'));
+
+
     showValues(day);
     showCompletedTasks(); // Atualiza a exibição das tarefas completadas
 }
 
 function showCompletedTasks() {
     let completedTasks = JSON.parse(localStorage.getItem('completed-tasks') || "[]");
-    let completedList = document.getElementById('completed-list'); // Ajuste o ID conforme sua estrutura HTML
+    let completedList = document.getElementById('completed-tasks'); 
     completedList.innerHTML = ''; // Limpa a lista antes de adicionar os itens
 
     for (let i = 0; i < completedTasks.length; i++) {
         completedList.innerHTML += `
             <li>
                 ${completedTasks[i]['name']}
-                <button onclick="removeCompletedTask(${i})">Remover</button>
+                <button id="btn-remove" onclick="removeCompletedTask(${i})">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/> <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                    </svg>
+                </button>
             </li>
         `;
     }
